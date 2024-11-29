c5a6270b4955a307d761e9b6ff48a6db22938553
sridhar anantha <vedsri1234@gmail.com>
2024-11-28 20:07:31 -0500

    added email field in the user profile,added redirect to login page in the login page when clicked on the logo

--- classnest_Base/models.py
+++ classnest_Base/models.py
@@ -1,7 +1,27 @@
 # classnest_Base/models.py
 from django.contrib.auth.models import User
 from django.db import models
+from django import forms
+from django.contrib.auth.forms import UserCreationForm
 
+class UserRegistrationForm(UserCreationForm):
+    USER_TYPE_CHOICES = [
+        ('student', 'Student'),
+        ('instructor', 'Instructor'),
+    ]
+    user_type = forms.ChoiceField(choices=USER_TYPE_CHOICES, label="I am a")
+    email = forms.EmailField(required=True, label="Email Address")
+
+    class Meta:
+        model = User
+        fields = ['username', 'email', 'password1', 'password2', 'user_type']
+
+    def save(self, commit=True):
+        user = super().save(commit=False)
+        user.email = self.cleaned_data['email']
+        if commit:
+            user.save()
+        return user
 
 class Course(models.Model):
     title = models.CharField(max_length=255)
