9ecfe6ea603db73bf3fec3e566e687d4c954a24f
sridhar anantha <vedsri1234@gmail.com>
2024-12-01 16:25:34 -0500

    fixed text message view overflow and registered inbox in admin page

--- classnest_Base/admin.py
+++ classnest_Base/admin.py
@@ -15,3 +15,13 @@ admin.site.register(Material)
 @admin.register(Profile)
 class ProfileAdmin(admin.ModelAdmin):
     list_display = ('user', 'github_link', 'linkedin_link')
+    
+
+
+@admin.register(Message)
+class MessageAdmin(admin.ModelAdmin):
+    list_display = ('sender', 'receiver', 'subject', 'is_read', 'timestamp')
+    list_filter = ('is_read', 'timestamp')
+    search_fields = ('subject', 'sender__username', 'receiver__username')
+    ordering = ('-timestamp',)
+    readonly_fields = ('timestamp',)
